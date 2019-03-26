91749307ec7b60cce7a5869e74ddb078a261d565
Jonathan Dunlap <jdunlap@outlook.com>
2019-03-25 22:16:15 -0700

    working build

--- src/index.js
+++ src/index.js
@@ -1,128 +1,153 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState, useRef, useImperativeHandle, forwardRef} from 'react';
 // import PropTypes from 'prop-types';
 import lottie from 'lottie-web';
 import lottieApi from 'lottie-api/dist/lottie_api';
 
 // LottieTypes
-export default function Lottie({ props }) {
+export default forwardRef(function Lottie(props, ref) {
+  const { eventListeners } = props;
+  ref = ref || React.createRef({});
+  const el = useRef(null);
+  const [anim, setAnim] = useState(null);
+  // const [animData, setAnimData] = useState(null);
+  const [animApi, setAnimApi] = useState(null);
+  // const [animationDataOld, setAnimationData] = useState(null);
+  
+
+  // Config
+  const {
+    loop,
+    autoplay,
+    rendererSettings,
+    segments,
+  } = props.options;
+  let animationData = props.options.animationData;
+  if(animationData && animationData.default) animationData = animationData.default;
+
+  const options = {
+    ...props.options,
+    container: el.current || null,
+    renderer: 'svg',
+    loop: loop !== false,
+    autoplay: autoplay !== false,
+    segments: segments !== false,
+    animationData,
+    rendererSettings,
+  };
+
+  const setSpeed = (_anim) => {
+    _anim = _anim || anim;
+    if(!_anim) return;
+    _anim.setSpeed(props.speed);
+  };
+
+  const setDirection = (_anim) => {
+    _anim = _anim || anim;
+    if(!_anim) return;
+    _anim.setDirection(props.direction);
+  };
+
+  const setAnimationControl = (_animApi) => {
+    _animApi = _animApi || animApi;
+    if(!_animApi) return;
+    const { animationControl } = props;
+    if (animationControl) {
+      const properties = Object.keys(animationControl);
+
+      properties.forEach((property) => {
+        const propertyPath = _animApi.getKeyPath(property);
+        const value = animationControl[property];
+        _animApi.addValueCallback(propertyPath, () => value);
+      });
+    }
+  };
+
+  const play = (_anim) => {
+    _anim = _anim || anim;
+    if(!_anim) return;
+    _anim.play();
+  };
+
+  const playSegments = (shouldForce) => {
+    if(!anim) return;
+    anim.playSegments(props.segments, shouldForce);
+  };
+
+  const stop = () => {
+    if(!anim) return;
+    anim.stop();
+  };
+
+  const pause = () => {
+    if(!anim) return;
+    if (props.isPaused && !anim.isPaused) {
+      anim.pause();
+    } else if (!props.isPaused && anim.isPaused) {
+      anim.pause();
+    }
+  };
+
+  const destroy = () => {
+    if(!anim) return;
+    anim.destroy();
+  };
+
+  const registerEvents = (eventListeners) => {
+    if(!eventListeners) return;
+    eventListeners.forEach((eventListener) => {
+      anim.addEventListener(eventListener.eventName, eventListener.callback);
+    });
+  };
+
+  const deRegisterEvents = (eventListeners) => {
+    if(!eventListeners) return;
+    eventListeners.forEach((eventListener) => {
+      anim.removeEventListener(eventListener.eventName, eventListener.callback);
+    });
+  };
+
   useEffect(() => {
-    const {
-      optionsIn,
-    } = props;
-    const { eventListeners } = props;
+    // options = { ...optionsIn, ...options  };
+   // if(!el.current) return;
 
-    const el = useRef(null);
+    options.container = el.current;
+    console.log('options', options);
+    const _anim = lottie.loadAnimation(options);
+    const _animApi = lottieApi.createAnimationApi(_anim);
+    setAnim(_anim);
+    setAnimApi(_animApi);
 
-    const {
-      loop,
-      autoplay,
-      animationData,
-      rendererSettings,
-      segments,
-    } = optionsIn;
-
-    let options = {
-      container: el.current,
-      renderer: 'svg',
-      loop: loop !== false,
-      autoplay: autoplay !== false,
-      segments: segments !== false,
-      animationData,
-      rendererSettings,
-    };
-
-    options = { ...options, ...optionsIn };
-
-    let anim = lottie.loadAnimation(options);
-
-    const setSpeed = () => {
-      anim.setSpeed(props.speed);
-    };
-
-    const setDirection = () => {
-      anim.setDirection(props.direction);
-    };
-
-    const setAnimationControl = () => {
-      const { animationControl } = props;
-      if (animationControl) {
-        const properties = Object.keys(animationControl);
-
-        properties.forEach((property) => {
-          const propertyPath = animApi.getKeyPath(property);
-          const value = animationControl[property];
-          animApi.addValueCallback(propertyPath, () => value);
-        });
-      }
-    };
-
-    const play = () => {
-      anim.play();
-    };
-
-    const playSegments = (shouldForce) => {
-      anim.playSegments(props.segments, shouldForce);
-    };
-
-    const stop = () => {
-      anim.stop();
-    };
-
-    const pause = () => {
-      if (props.isPaused && !anim.isPaused) {
-        anim.pause();
-      } else if (!props.isPaused && anim.isPaused) {
-        anim.pause();
-      }
-    };
-
-    const destroy = () => {
-      anim.destroy();
-    };
-
-    const registerEvents = (eventListeners) => {
-      eventListeners.forEach((eventListener) => {
-        anim.addEventListener(eventListener.eventName, eventListener.callback);
-      });
-    };
-
-    const deRegisterEvents = (eventListeners) => {
-      eventListeners.forEach((eventListener) => {
-        anim.removeEventListener(eventListener.eventName, eventListener.callback);
-      });
-    };
-
-
-    setSpeed();
-    setDirection();
-    animApi = lottieApi.createAnimationApi(anim);
-    registerEvents(eventListeners);
-    setAnimationControl();
+    setSpeed(_anim);
+    setDirection(_anim);
+    registerEvents(eventListeners, _anim);
+    setAnimationControl(_animApi);
 
     return () => {
       deRegisterEvents(props.eventListeners);
       destroy();
       options.animationData = null;
-      anim = null;
-      animApi = null;
-    }
-  }, []);
+      // setAnim(null);
+      // setAnimApi(null);
+    };
+  }, [el, options.animationData]);
 
-  // componentWillUpdate(nextProps
-  useEffect( () => {
-    /* Recreate the animation handle if the data is changed */
-    if (options.animationData !== nextProps.options.animationData) {
-      deRegisterEvents(props.eventListeners);
-      destroy();
-      options = { ...options, ...nextProps.options };
-      anim = lottie.loadAnimation(options);
-      animApi = lottieApi.createAnimationApi(anim);
-      registerEvents(nextProps.eventListeners);
-    }
-  });
+  useImperativeHandle(ref, () => ({
+    play: play,
+    registerEvents: registerEvents,
+    deRegisterEvents: deRegisterEvents,
+    setSpeed: setSpeed,
+    setDirection: setDirection,
+    anim: anim, 
+    animApi: animApi,
+    playSegments: playSegments,
+    pause: pause,
+    destroy: destroy
+  }));
 
   // componentDidUpdate
   useEffect( () => {
+    if(!el.current) return;
+
     if (props.isStopped) {
       stop();
     } else if (props.segments) {
@@ -178,9 +203,9 @@ export default function Lottie({ props }) {
       role={ariaRole}
       aria-label={ariaLabel}
       tabIndex="0"
-    />
+    >test</div>
   );
-}
+});
 
 /*
 ref={(c) => {
