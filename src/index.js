689fc9a6d6a5b7bd7242091704db184c2cd5acfd
Felippe Nardi <felippe@nardi.me>
2020-06-10 16:18:06 -0300

    Fix: animation reseting on remount and segment not respected

--- src/index.js
+++ src/index.js
@@ -39,6 +39,14 @@ export default class Lottie extends React.Component {
     this.animApi = lottieApi.createAnimationApi(this.anim);
     this.registerEvents(eventListeners);
     this.setAnimationControl();
+
+    if (this.props.isStopped) {
+      this.stops();
+    } else if (this.props.segments) {
+      this.playSegments(true);
+    } else {
+      this.play();
+    }
   }
 
   componentWillUpdate(nextProps /* , nextState */) {
@@ -53,12 +61,17 @@ export default class Lottie extends React.Component {
     }
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
     if (this.props.isStopped) {
       this.stop();
     } else if (this.props.segments) {
-      const shouldForce = !!this.props.forceSegments;
-      this.playSegments(shouldForce);
+      if (
+        JSON.stringify(this.props.segments) ===
+        JSON.stringify(prevProps.segments)
+      ) {
+        return;
+      }
+      this.playSegments(this.props.forceSegments);
     } else {
       this.play();
     }
